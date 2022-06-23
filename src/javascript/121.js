63e10c351a648ff480deb540c40fc47999190d10
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-23 14:18:54 -0500

    121 v2

--- src/javascript/121.js
+++ src/javascript/121.js
@@ -13,19 +13,14 @@ var maxProfit = function (prices) {
         return 0;
     }
 
-    for (let i = 1; i < prices.length; i++) {
+    for (let i = 0; i < prices.length; i++) {
         let tempProfit = prices[sell] - prices[buy];
 
-        if (prices[i] > prices[sell]) {
-            sell = i;
-        }
-
-        if (prices[i] <= prices[buy] && i !== prices.length - 1) {
+        if (tempProfit <= 0) {
             buy = i;
-        }
-
-        if (sell < buy) {
-            sell = i;
+            sell = i + 1;
+        } else if (tempProfit > 0) {
+            sell++;
         }
 
         profit = tempProfit > profit ? tempProfit : profit;
@@ -38,7 +33,8 @@ console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 console.log(maxProfit([7, 6, 4, 3, 1])); // 0
 console.log(maxProfit([1, 2])); // 1
 console.log(maxProfit([2, 4, 1])); // 2
-// console.log(maxProfit([2, 1, 2, 1, 0, 1, 2])); // 2
-// console.log(maxProfit([3, 3, 5, 0, 0, 3, 1, 4])); // 4
+console.log(maxProfit([2, 1, 2, 1, 0, 1, 2])); // 2
+console.log(maxProfit([3, 3, 5, 0, 0, 3, 1, 4])); // 4
 console.log(maxProfit([0])); // 0
 console.log(maxProfit([3, 2, 6, 5, 0, 3])); // 4
+console.log(maxProfit([1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9])); // 9
